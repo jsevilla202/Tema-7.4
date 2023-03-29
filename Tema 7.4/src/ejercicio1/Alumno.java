fa260b841ed060062a73cce7a148a8d44403e769
jsevilla202 <jsevilla@iesnervion.es>
2023-03-29 14:36:08 +0200

    Alumno

--- Tema 7.4/src/ejercicio1/Alumno.java
+++ Tema 7.4/src/ejercicio1/Alumno.java
@@ -1,5 +1,63 @@
 package ejercicio1;
 
 public class Alumno {
-
+	/**
+	 * Nombre del alumno
+	 */
+	private String nombre;
+	/**
+	 * Nota MEdia del alumno
+	 */
+	private double notaMedia;
+	/**
+	 * Constructor por defecto
+	 */
+	public Alumno() {
+		super();
+	}
+	/**
+	 * Constructor con todos los parametros
+	 * @param nombre Nombre del alumno dado por el usuario
+	 * @param notaMedia Nota media del alumno dada por el usuario
+	 */
+	public Alumno(String nombre, double notaMedia) {
+		super();
+		this.nombre = nombre;
+		this.notaMedia = notaMedia;
+	}
+	/**
+	 * Getter de Nombre
+	 * @return Devuelve el nombre del alumno
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+	/**
+	 * Setter de Nombre
+	 * @param nombre Valor del nombre dado por el usuario
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	/**
+	 * Getter de Nota Media
+	 * @return Devuelve el valor de la nota media
+	 */
+	public double getNotaMedia() {
+		return notaMedia;
+	}
+	/**
+	 * Setter de Nota Media
+	 * @param notaMedia Valor de la nota media dada por el usuario
+	 */
+	public void setNotaMedia(double notaMedia) {
+		this.notaMedia = notaMedia;
+	}
+	
+	@Override
+	public String toString() {
+		return "Alumno [nombre=" + nombre + ", notaMedia=" + notaMedia + "]";
+	}
+	
+	
 }
--- Tema 7.4/src/ejercicio1/Main.java
+++ Tema 7.4/src/ejercicio1/Main.java
@@ -3,8 +3,20 @@ package ejercicio1;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Alumno[] alumnos = new Alumno[30];
 
+		int opcion = 0;
+		String nombre;
+		double notaMedia;
+
+		while (opcion != 5) {
+			System.out.print("ALUMNOS/AS\n" + "===================="
+					+ " 1. Listado.\n 2. Nuevo Alumno\n 3. Modificar\n 4. Borrar\n 5. Salir" + "¿Qué desea hacer? ");
+			
+			switch(opcion) {
+			
+			}
+		}
 	}
 
 }
